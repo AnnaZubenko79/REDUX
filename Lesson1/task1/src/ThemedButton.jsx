8a5d78a09b77cad647b2863dda58228e3b7c59bf
Anna Zubenko <annazubenko79@gmail.com>
2022-10-06 13:32:47 +0300

    change

--- Lesson1/task1/src/ThemedButton.jsx
+++ Lesson1/task1/src/ThemedButton.jsx
@@ -5,12 +5,12 @@ class ThemedButton extends React.Component {
   render() {
     return (
       <button
-        {...this.props}
-        className="btn"
         style={{
           backgroundColor: this.context.background,
           color: this.context.fontColor,
         }}
+        {...this.props}
+        className="btn"
       />
     );
   }
